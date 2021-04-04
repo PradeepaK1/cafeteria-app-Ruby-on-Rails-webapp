371d21a72d9a3bc42c9626c7c1fbcb86fc99f63d
PradeepaK1 <ptljkpd@gmail.com>
2021-04-05 00:28:36 +0530

    Pushing Code

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,6 @@
+class ApplicationController < ActionController::Base
+    include ApplicationHelper
+    http_basic_authenticate_with name: 'admin', password: 'admin', except: [:index, :show]
+
+
+end
