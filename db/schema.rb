65f275bdb810006eb305d4b829b6ce101f58542e
PradeepaK1 <ptljkpd@gmail.com>
2021-04-06 21:56:39 +0530

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_28_150942) do
+ActiveRecord::Schema.define(version: 2021_04_01_150942) do
 
   create_table "assignments", force: :cascade do |t|
     t.integer "color_id"
